9b2243f01f7de6a73b7552f9bcaffce9aeb849c8
KOUMEDZRO Oréliau <oreliauk@gmail.com>
2018-04-19 05:03:46 +0200

    add popup and finish project

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ let count = document.querySelector(".move");
  *This function close cards and shuffle the game
  */
 
-restart.addEventListener('click', function(){
+restart.addEventListener('click', function reset(){
 	for(let i = 0 ; i < cards.length; i++){
 		cards[i].classList.remove("match", "open", "show");
 	}
@@ -25,6 +25,9 @@ restart.addEventListener('click', function(){
 		const replace = document.innerHTML ="<i class =\"" + rep[j] + "\"></i>";
 		cards[j].firstElementChild.outerHTML = replace;
 	}
+
+	move = 0;
+	count.innerHTML = `number of moves : ${move}`;
 });
 
 
@@ -37,15 +40,18 @@ for(let i = 0 ; i < cards.length; i++){
 cards[i].addEventListener("click", function() {
 	cards[i].classList.add("show", "open");
 	const show = document.querySelectorAll(".show")
- 
 
-	if(show.length = 2){
+
+	if(show.length == 2){
 
 		if(show[0].innerHTML === show[1].innerHTML){
 		show[0].classList.add("match");
-		show[1].classList.add("match")
+		show[1].classList.add("match");
+		show[0].classList.remove("show");
+		show[1].classList.remove("show");
 			move += 1;
-			return move;}
+			return move;
+		;}
 
 		else{setTimeout(function(){show[0].classList.remove("show", "open");
 			show[1].classList.remove("show", "open")}, 500);
@@ -53,11 +59,13 @@ cards[i].addEventListener("click", function() {
 			return move;};
 	};
 
-	if(show.length = 4){
+	if(show.length == 4){
 
 		if(show[2].innerHTML === show[3].innerHTML){
 		show[2].classList.add("match");
-		show[3].classList.add("match")	
+		show[3].classList.add("match");
+		show[2].classList.remove("show");
+		show[3].classList.remove("show");
 			move += 1;
 			return move;}
 
@@ -67,11 +75,13 @@ cards[i].addEventListener("click", function() {
 			return move;};
 	};
 
-	if(show.length = 6){
+	if(show.length == 6){
 
 		if(show[4].innerHTML === show[5].innerHTML){
 		show[4].classList.add("match");
-		show[5].classList.add("match")
+		show[5].classList.add("match");
+		show[4].classList.remove("show");
+		show[5].classList.remove("show");
 			move += 1;
 			return move;}
 
@@ -81,11 +91,13 @@ cards[i].addEventListener("click", function() {
 			return move;;};
 	};
 
-	if(show.length = 8){
+	if(show.length == 8){
 
 		if(show[6].innerHTML === show[7].innerHTML){
 		show[6].classList.add("match");
-		show[7].classList.add("match")
+		show[7].classList.add("match");
+		show[6].classList.remove("show");
+		show[7].classList.remove("show");
 			move += 1;
 			return move;}
 
@@ -95,11 +107,13 @@ cards[i].addEventListener("click", function() {
 			return move;;};
 	};
 
-	if(show.length = 10){
+	if(show.length == 10){
 
 		if(show[8].innerHTML === show[9].innerHTML){
 		show[8].classList.add("match");
-		show[9].classList.add("match")
+		show[9].classList.add("match");
+		show[9].classList.remove("show");
+		show[8].classList.remove("show");
 			move += 1;
 			return move;}
 
@@ -109,11 +123,13 @@ cards[i].addEventListener("click", function() {
 			return move;;};
 	};
 
-	if(show.length = 12){
+	if(show.length == 12){
 
 		if(show[10].innerHTML === show[11].innerHTML){
 		show[10].classList.add("match");
-		show[11].classList.add("match")
+		show[11].classList.add("match");
+		show[10].classList.remove("show");
+		show[11].classList.remove("show");
 			move += 1;
 			return move;}
 
@@ -123,11 +139,13 @@ cards[i].addEventListener("click", function() {
 			return move;;};
 	};
 
-	if(show.length = 14){
+	if(show.length == 14){
 
 			if(show[12].innerHTML === show[13].innerHTML){
 			show[12].classList.add("match");
-			show[13].classList.add("match")
+			show[13].classList.add("match");
+			show[12].classList.remove("show");
+			show[13].classList.remove("show");
 				move += 1;
 				return move;}
 
@@ -137,15 +155,17 @@ cards[i].addEventListener("click", function() {
 				return move;;};
 		};
 
-	if(show.length = 16){
+	if(show.length == 16){
 
 			if(show[14].innerHTML === show[15].innerHTML){
 			show[14].classList.add("match");
 			show[15].classList.add("match");
+			show[14].classList.remove("show");
+			show[15].classList.remove("show");
 				move += 1;
 				return move;
-			fu
 		}
+
 		};
 
 
@@ -177,7 +197,6 @@ var start = 0
 var end = 0
 var diff = 0
 var timerID = 0
-deck.addEventListener('click', chronoStart);
 restart.addEventListener('click', chronoStart);
 function chrono(){
 	end = new Date()
@@ -206,19 +225,18 @@ function chronoStart(){
 	start = new Date()
 	chrono()
 };
-
+chronoStart();
 /*
  *This is Move Counter
  */
-deck.addEventListener('click', function(event){
-	 event.preventDefault();
+deck.addEventListener('click', function(){
+
 	count.innerHTML = `number of moves : ${move}`;
 	});
 
 /*
  *Star Rating
  */
- /*Selector of start*/
 const stars = document.querySelector(".stars");
 
 const star1 = document.querySelector(".star1").firstElementChild;
@@ -232,16 +250,112 @@ restart.addEventListener('click', function(){
 	difficult();
 });
 
+
+/*
+ *change star if you do to much move
+ */
 function difficult(){
-	if (move >= 20){
+	if(move < 20){
+		star1.style.color = "black";
+		star2.style.color = "black";
+		star3.style.color = "black";
+	}
+	else if (move < 30){
 		star3.style.color = "grey";
+		star1.style.color = "black";
+		star2.style.color = "black";
 	}
-	else if (move >= 30){
+	else if (move < 40){
+		star3.style.color = "grey";
 		star2.style.color = "grey";
+		star1.style.color = "black";
 	}
-	else if (move >= 40){
+	else{
 		star1.style.color ="grey"
+		star2.style.color = "grey";
+		star3.style.color = "grey";
 	};
 };
 
 
+/*
+ *pop up
+ */
+const congrat = document.querySelector(".congrat");
+const finalTime = document.querySelector("#chronotime").value;
+
+deck.addEventListener('click', function(){
+	const match = document.querySelectorAll('.match');
+if(match.length == 16){popup();}
+});
+
+let popup = function final(){
+	congrat.style.display = "initial";
+	lvl();
+	congrat.innerHTML = `Well done!!! <br>
+	You did it in : ${finalTime} <br>
+	Your level is : ${level} stars <br>`;
+	addbutton();
+};
+
+/*
+ * function to get the value of the level
+ */
+
+let level = 0;
+function lvl(){
+	if (move < 20){
+		level += 3;
+		return level;
+	}
+	else if (move >= 20){
+		level += 2;
+		return level;	 
+	}
+	else if (move >= 30){
+		level += 1;
+		return level;
+	}
+	else if (move >= 40){
+		level += 0;
+		return level;
+	};
+};
+
+/*
+ * function for creat button
+ */
+
+function addbutton(){
+	const button = document.createElement('input');
+	button.type="submit";
+	button.value="Play Again";
+	button.className="button";
+	congrat.appendChild(button);
+	const mybutton = document.getElementsByClassName("button");
+	button.addEventListener("click",function(){again();});
+	};
+
+
+
+/*
+ * event for click on "play again"
+ */
+
+
+function again(){
+	for(let i = 0 ; i < cards.length; i++){
+		cards[i].classList.remove("match", "open", "show");
+	}
+
+	const rep = shuffle(array);
+
+	for(let j = 0; j < cards.length; j++){
+		const replace = document.innerHTML ="<i class =\"" + rep[j] + "\"></i>";
+		cards[j].firstElementChild.outerHTML = replace;
+	}
+
+	move = 0;
+	count.innerHTML = `number of moves : ${move}`;
+	congrat.style.display = "none";
+};
